c1e0b2947848d11c26274b1637a6f413ecd4b302
BassaniDavid <davbassan98@gmail.com>
2025-03-26 14:54:38 +0100

    feat: route "bacheca" created

--- arrayPost.js
+++ arrayPost.js
@@ -3,31 +3,31 @@ const arrayPost = [
         titol: 'ciambella',
         content:'è proprio una ciambella',
         img:'images/ciambellone.jpeg',
-        tags:'ciambella'
+        tags: ['ciambella']
     },
     {
         titol: 'cracker',
         content:'ottimo cracker di barbabietola',
         img:'images/cracker_barbabietola.jpeg',
-        tags:'cracker'
+        tags:['cracker']
     },
     {
         titol: 'pane fritto',
         content:'ottimo pane fritto dolce',
         img:'images/pane_fritto_dolce.jpeg',
-        tags:'pane fritto'
+        tags:['pane fritto']
     },
     {
         titol: 'pasta',
         content:'deliziosa pasta di barbabietola',
         img:'images/pasta_barbabietola.jpeg',
-        tags:'pasta barbabietola'
+        tags:['pasta barbabietola']
     },
     {
         titol: 'torta',
         content:'ottima torta paesana',
         img:'images/torta_paesana.jpeg',
-        tags:'torta paesana'
+        tags:['torta paesana']
     }
 ]
 
--- main.js
+++ main.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
 
+app.get('/bacheca', (req, res) => {
+    res.json(posts)
+})
+
 app.listen(port, () => {
     console.log('il server ha ricevuto una domanda')
 })
