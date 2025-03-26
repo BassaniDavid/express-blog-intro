7ae45a34599d3dc811704d31b9565b176c379429
BassaniDavid <davbassan98@gmail.com>
2025-03-26 14:51:38 +0100

    feat: js file with posts' array created

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 2000
 
+const posts = require('./arrayPost.js')
+
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
